551e8ee9bb9cc18fa6dd330290c03e87a6e56fb7
harol <harol@ALONSO-DIEHL>
2022-08-27 23:06:00 -0500

    Proyecto con autenticación con cuenta de google

---
+++ SpringBootSocialJPA/src/main/java/com/hrojas/essalud/SpringBootSocialJpaApplication.java
@@ -0,0 +1,13 @@
+package com.hrojas.essalud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSocialJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSocialJpaApplication.class, args);
+	}
+
+}
---
+++ SpringBootSocialJPA/src/test/java/com/hrojas/essalud/SpringBootSocialJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrojas.essalud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSocialJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
